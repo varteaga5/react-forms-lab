fb3b53ac8bf4af339a4486bf596db998047e6d15
vincent <vincent.arteaga@gmail.com>
2021-06-20 10:54:44 -0700

    commit tree d2eddb5a2880776e17977869f3eb95fec23cea4c

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,58 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
+  handleUChange = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+  handlePChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // const uname = "timy";
+    // const pname = "himy";
+    // this.props.handleLogin(uname, pname);
+
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(this.state.username, this.state.password);
+    }
+  };
 
   render() {
+    console.log("props", this.props.handleLogin);
     return (
-      <form>
+      <form onSubmit={(event) => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              onChange={(event) => this.handleUChange(event)}
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              onChange={(event) => this.handlePChange(event)}
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
