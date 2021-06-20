4953443899e5ee357de6ba2796e3691ca899b3ce
vincent <vincent.arteaga@gmail.com>
2021-06-20 12:16:03 -0700

    assignment complete

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: "",
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      tweet: event.target.value,
+    });
+    console.log(event);
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          onChange={(event) => this.handleChange(event)}
+          name="message"
+          id="message"
+          value={this.state.tweet}
+        />
+        <h2>Remaining characters</h2>
+        <p>{this.props.maxChars - this.state.tweet.length}</p>
       </div>
     );
   }
